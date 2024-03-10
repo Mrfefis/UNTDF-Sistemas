188960d500493fe9ac653445a7d595e5576a20b4
Mrfefis <https6mil@gmail.com>
2024-03-10 12:41:47 -0300

    Continuacion del Ejercicio 2 Capitulo 2 Libro Ulman

--- Programacion II/Libro Ulman/Capitulo 2/Ejercicio 2/UlistaArreglo.pas
+++ Programacion II/Libro Ulman/Capitulo 2/Ejercicio 2/UlistaArreglo.pas
@@ -9,37 +9,86 @@ INTERFACE
 
 		Tindice = integer;
 
-		TlistaArreglo = record
+		Tlista = record
 			elementos: array[1..n] of Telemento;
 			fin: Tindice;
 		end;
 
-	procedure creararreglo(var lista: TlistaArreglo);
+	procedure crear(var lista: Tlista);
 
-	procedure agregar(var lista: TlistaArreglo; indice: Tindice; elemento: Telemento);
+	procedure agregar(var lista: Tlista; indice: Tindice; elemento: Telemento);
 
-	function primero(lista: TlistaArreglo): Tindice;
+	procedure eliminar(var lista: Tlista; indice: Tindice);
+
+	function recuperar(var lista: Tlista; indice: Tindice): Telemento;
+
+	function localizar(var lista: Tlista; elemento: Telemento): Tindice;
+
+	function primero(var lista: Tlista): Tindice;
 
 	function siguiente(indice: Tindice): Tindice;
 
+	function fin(var lista: Tlista): Tindice;
+
 IMPLEMENTATION
 
-	procedure creararreglo(var lista: TlistaArreglo);
+	procedure crear(var lista: Tlista);
 	begin
 		lista.fin:= 1;
 	end;
 
-	procedure agregar(var lista: TlistaArreglo; indice: Tindice; elemento: Telemento);
+	procedure agregar(var lista: Tlista; indice: Tindice; elemento: Telemento);
 	var
 		i: Tindice;
 	begin
-		for i:= lista.fin to indice do
-			lista.elementos[i]:= lista.elementos[i-1];
-		inc(lista.fin);
-		lista.elementos[indice]:= elemento;
+		if (lista.fin>=n) then
+			writeln('Error(Agregar): Lista llena')
+		else begin
+			for i:= lista.fin-1 downto indice do
+				lista.elementos[i+1]:= lista.elementos[i];
+			inc(lista.fin);
+			lista.elementos[indice]:= elemento;
+		end
 	end;
 
-	function primero(lista: TlistaArreglo): Tindice;
+	procedure eliminar(var lista: Tlista; indice: Tindice);
+	var
+		i: Tindice;
+	begin
+		if (indice<1) or (indice>lista.fin) then
+			writeln('Error(Eliminar): La posicion no existe')
+		else begin
+			dec(lista.fin);
+			for i:= indice to lista.fin-1 do begin
+				lista.elementos[i]:= lista.elementos[i+1]
+			end
+		end
+	end;
+
+	function recuperar(var lista: Tlista; indice: Tindice): Telemento;
+	begin
+		if (indice<1) or (indice>=lista.fin) then
+			writeln('Error(Recuperar): La posicion no existe')
+		else recuperar:= lista.elementos[indice]
+	end;
+
+	function localizar(var lista: Tlista; elemento: Telemento): Tindice;
+	var
+		i: Tindice;
+		encontrado: boolean;
+	begin
+		i:= primero(lista);
+		encontrado:= false;
+		while (i<>fin(lista)) and not encontrado do begin
+			if (recuperar(lista, i)=elemento) then
+				encontrado:= true
+			else i:= siguiente(i)
+		end;
+		if encontrado then localizar:= i
+		else localizar:= -1
+	end;
+
+	function primero(var lista: Tlista): Tindice;
 	begin
 		primero:= 1;
 	end;
@@ -49,7 +98,7 @@ IMPLEMENTATION
 		siguiente:= indice + 1;
 	end;
 
-	function fin(lista: TlistaArreglo): Tindice;
+	function fin(var lista: Tlista): Tindice;
 	begin
 		fin:= lista.fin;
 	end;
--- Programacion II/Libro Ulman/Capitulo 2/Ejercicio 2/main.pas
+++ Programacion II/Libro Ulman/Capitulo 2/Ejercicio 2/main.pas
@@ -1,5 +1,5 @@
 program main;
-uses crt, UlistaArreglo, UlistaApuntadores, UlistaCursores;
+uses crt, UlistaArreglo;
 {
 	Consigna:
 	Escríbanse programas para insertar, suprimir y localizar un elemento en una
@@ -9,40 +9,83 @@ uses crt, UlistaArreglo, UlistaApuntadores, UlistaCursores;
 	c) Cursores
 	¿Cuál es el tiempo de ejecución de cada uno de estos programas?
 }
+const
+	randlim = 15;
+
 // ---------------------------------SUBPROGRAMAS---------------------------------
 
-procedure CrearListas(var listaArreglo: TlistaArreglo;
-					  var listaPuntero: TlistaApuntadores;
-					  var listaCursor: TlistaCursores);
+procedure MostrarLista(var lista: Tlista);
+var
+	i: Tindice;
 begin
-	creararreglo(listaArreglo);
-	crearpuntero(listaPuntero);
-	crearcursor(listaCursor);
+	write('[');
+	i:= primero(lista);
+	if (i<>fin(lista)) then begin
+		while (siguiente(i)<>fin(lista)) do begin
+			write(recuperar(lista, i), ', ');
+			i:= siguiente(i)
+		end;
+		write(recuperar(lista, i))
+	end;
+	writeln(']');
 end;
 
-procedure CargarListaArreglo(var lista: TlistaArreglo);
+procedure LocalizarLista(var lista: Tlista);
 var
 	i: integer;
-	p: Tindice;
+	aux: Telemento;
+	posicion: Tindice;
 begin
-	p:= primero(lista);
-	for i:= 1 to 10 do begin
-		agregar(lista, p, random(80));
-		p:= siguiente(p)
-	end;
+	write('Elemento: '); readln(aux);
+	posicion:= localizar(lista, aux);
+	if posicion=-1 then
+		writeln(aux, ' no esta en la lista')
+	else writeln(aux, ' esta en la posicion ', posicion)
+end;
+
+procedure Presentacion;
+begin
+	clrscr;
+	writeln;
+	writeln('-----------------------------------------------------');
+	writeln('Operaciones de listas con Diferentes Implementaciones');
+	writeln('-----------------------------------------------------');
+	writeln;
+end;
+
+procedure Opciones(var opcion: char);
+begin
+	writeln;
+	writeln('1. Cargar Elementos.');
+	writeln('2. Suprimir elemento.');
+	writeln('3. Localizar elemento.');
+	writeln('4. Terminar.');
+	writeln;
+	write('Opcion: '); readln(opcion)
 end;
 
 // ------------------------------PROGRAMA PRINCIPAL------------------------------
 
 var
-	listaArreglo: TlistaArreglo;
-	listaPuntero: TlistaApuntadores;
-	listaCursor: TlistaCursores;
+	lista: Tlista;
+	opcion: char;
 begin
-	CrearListas(listaArreglo, listaPuntero, listaCursor);
-	// a)
-	CargarListaArreglo(listaArreglo);
-	{SuprimirListaArreglo(listaArreglo);
-	LocalizarListaArreglo(listaArreglo);}
-	readkey
+	Randomize;
+	crear(lista);
+	repeat
+		Presentacion;
+		MostrarLista(lista);
+		Opciones(opcion);
+		case opcion of
+			'1': agregar(lista, fin(lista), random(randlim));
+			'2': eliminar(lista, fin(lista));
+			'3': LocalizarLista(lista);
+			'4': ;
+			else begin
+				writeln('Error: elige una opcion correcta');
+				writeln('Presiona cualquier tecla para continuar...');
+				readkey;
+			end
+		end
+	until (opcion='4')
 end.
